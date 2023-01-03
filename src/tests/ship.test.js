ecf15277a019165dd3fc1925799426dca9161265
woftis <stevenwalker1991@googlemail.com>
2023-01-03 15:53:52 +0000

    set up basic ship factory with some starter functions and test cases

---
+++ src/tests/ship.test.js
@@ -0,0 +1,26 @@
+import { Ship } from '../functions/ship.js';
+
+test('check ship object has properties', () => {
+  expect(Ship(5).isSunk()).toBe(false);
+  expect(Ship(5)).toHaveProperty('length', 5);
+  expect(Ship(5)).toHaveProperty('hits', 0);
+})
+
+test('hits increase on the boat', () => {
+  const battleShip = Ship(5);
+  battleShip.hit();
+
+  expect(battleShip.hits).toBe(1);
+  expect(battleShip.isSunk()).toBe(false);
+})
+
+test('isSunk is true once hits amount to length', () => {
+  const battleShip = Ship(10);
+  for (let i = 0; i< 10; i++) {
+    battleShip.hit();
+  }
+
+  expect(battleShip.isSunk()).toBe(true);
+  expect(battleShip.hits).toBe(10);
+
+})
