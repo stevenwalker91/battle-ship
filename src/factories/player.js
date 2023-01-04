4ce7273a55e0535466d171932b5bccfb7563c5ad
woftis <stevenwalker1991@googlemail.com>
2023-01-04 14:13:41 +0000

    added player factory, computer factory and tests

---
+++ src/factories/player.js
@@ -0,0 +1,29 @@
+import { Gameboard } from '../factories/gameboard.js';
+
+export const Player = (name) => {
+  
+  const makeAttack = (coords, enemyBoard) => {
+    enemyBoard.receiveAttack(coords);
+  }
+
+  return {
+    name,
+    makeAttack
+  }
+}
+
+export const Computer = (name) =>  {
+
+  const dumbComputerAttack = (playerBoard) => {
+    const availableMoves = playerBoard.getAvailableMoves();
+    const numberOfMoves = availableMoves.length;
+    const selectedMove = Math.floor(Math.random() * numberOfMoves);
+    playerBoard.receiveAttack(availableMoves[selectedMove]);
+
+  }
+
+  return Object.assign({}, Player(name), {
+    dumbComputerAttack
+  });
+
+}
---
+++ src/tests/player.test.js
@@ -0,0 +1,55 @@
+import { Player, Computer } from '../factories/player.js';
+import { Gameboard } from '../factories/gameboard.js';
+
+let playerOne = Player('Player One');
+let computerPlayer = Computer('AI Computer');
+
+
+describe('can create players', () => {
+  test('player object created with correct attributes', () => {
+
+
+    expect(playerOne).toHaveProperty('name', 'Player One')
+    expect(playerOne).toHaveProperty('makeAttack')
+  })
+
+  test('computer object created with correct attributes', () => {
+
+    expect(computerPlayer).toHaveProperty('name', 'AI Computer')
+    expect(computerPlayer).toHaveProperty('makeAttack')
+    expect(computerPlayer).toHaveProperty('dumbComputerAttack')
+
+  })
+})
+
+describe('each player can attack', () => {
+  let playerBoard;
+  let enemyBoard;
+
+  beforeEach(() => {
+    playerBoard = Gameboard();
+    enemyBoard = Gameboard();
+    jest.spyOn(global.Math, 'random').mockReturnValue(0.19);
+  })
+
+  afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore();
+})
+
+  test('player attacks land on the enemy board', () => {
+    playerOne.makeAttack([1,2], enemyBoard);
+
+    expect(enemyBoard.gameboard[1][2]).toBe('miss');
+    expect(playerBoard.gameboard[1][2]).toBe(null);
+
+  })
+
+  test('computer moves land on the player board', () => {
+    computerPlayer.dumbComputerAttack(playerBoard);
+
+    expect(playerBoard.gameboard[1][2]).toBe('miss');
+    expect(enemyBoard.gameboard[1][2]).toBe(null);
+
+    console.log(playerBoard.gameboard)
+  })
+})
