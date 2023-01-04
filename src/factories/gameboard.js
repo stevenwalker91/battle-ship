b1ab0690f9b2f51a0f03891569d3ff0e89927e4f
woftis <stevenwalker1991@googlemail.com>
2023-01-04 09:59:12 +0000

    added ability for gameboard to receive attacks and suitable test cases

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,5 +1,8 @@
+import { Ship } from './ship.js';
+
 
 export const Gameboard = () => {
+  // gameboard tracks all misses or hits
   let gameboard = [
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
@@ -8,33 +11,74 @@ export const Gameboard = () => {
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null]
-  ]
+  ];
 
-const placeShip = (coords, shipType) => {
-  
-  // first iterate over the coordinates and check there is no ship there already
-  for (const coord of coords) {
-    if (!_checkCoordEmpty(coord)) throw `There's already a ship here.`;
+  //ship positions tracks where each boat is
+  const shipPositions = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null]
+  ];
+
+  const _checkCoordEmpty = coord => {
+    
+    if (shipPositions[coord[0]][coord[1]] === null) {
+      return true;
+    }
+    return false;
   }
-  
-  for (const coord of coords) {
-    gameboard[coord[0]][coord[1]] = shipType;
+
+  const _checkCoordLegal = coord => {
+    const gameBoardPiece = gameboard[coord[0]][coord[1]];
+
+    if (gameBoardPiece === null) {
+      return true;
+    };
+    return false;
   }
-}
 
-const _checkCoordEmpty = (coord) => {
-  if (gameboard[coord[0]][coord[1]] === null) {
-    return true;
+  const placeShip = (coords, shipType) => {
+    
+    const shipSize = coords.length;
+
+    // first iterate over the coordinates and check there is no ship there already
+    for (const coord of coords) {
+      if (!_checkCoordEmpty(coord)) throw `There's already a ship here.`;
+    }
+
+    const newShip = Ship(shipSize);
+    
+    for (const coord of coords) {
+      shipPositions[coord[0]][coord[1]] = newShip;
+    }
   }
-  return false;
-}
 
-return {
-  get gameboard() {
-    return gameboard;
-  },
-  placeShip
+  const receiveAttack = (coord) => {
+    if (!_checkCoordLegal(coord)) throw `You've already made that move.`;
 
-}
+    if (_checkCoordEmpty(coord)) {
+      gameboard[coord[0]][coord[1]] = 'miss';
+    } else {
+      gameboard[coord[0]][coord[1]] = 'hit';
+      shipPositions[coord[0]][coord[1]].hit();
+    
+    }
+  }
+
+
+  return {
+    get gameboard() {
+      return gameboard;
+    },
+    get shipPositions() {
+      return shipPositions;
+    },
+    placeShip,
+    receiveAttack
+  }
 
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,6 +1,6 @@
 import { Gameboard } from '../factories/gameboard.js';
 
-let gameBoardOne; 
+let gameBoardOne;
 
 beforeEach(() => {
   gameBoardOne = Gameboard();
@@ -12,22 +12,47 @@ test('gameboard is set up with appropriate size', () => {
   expect(gameBoardOne.gameboard[6]).toHaveLength(7);
 })
 
-describe('place ships test cases', () => {
+describe('place ships on the gameboard', () => {
 
-  test('place ship adds ship to correct coords', () => {
+  beforeEach(() => {
     gameBoardOne.placeShip([[1,2],[1,3],[1,4]], 'battleship')
-  
-    expect(gameBoardOne.gameboard[1][2]).toBe('battleship');
-    expect(gameBoardOne.gameboard[1][3]).toBe('battleship');
-    expect(gameBoardOne.gameboard[1][4]).toBe('battleship');
-  
+    gameBoardOne.placeShip([[2,2],[2,3]], 'carrier')
   })
+  
+  test('place ship adds ship to correct coords', () => {
+  
+    expect(gameBoardOne.shipPositions[1][2]).toHaveProperty('length', 3);
+    expect(gameBoardOne.shipPositions[1][3]).toHaveProperty('length', 3);
+    expect(gameBoardOne.shipPositions[1][4]).toHaveProperty('length', 3);
+    expect(gameBoardOne.shipPositions[2][3]).toHaveProperty('length', 2);
+  
+  });
 
   test('ships cant overwrite other ships', () => {
-    gameBoardOne.placeShip([[1,2],[1,3],[1,4]], 'battleship');
-
     expect(() => gameBoardOne.placeShip([[1,2],[1,3],[1,4]], 'carrier')).toThrow(`There's already a ship here.`);
+  });
+})
+
+describe('gameboard can receive attacks', () => {
+
+  beforeEach(() => {
+    gameBoardOne.placeShip([[1,2],[1,3],[1,4]], 'battleship');
+    gameBoardOne.receiveAttack([1,2]);
+  })
+  
+  test('ship hits property is updated', () => {
+    expect(gameBoardOne.shipPositions[1][2]).toHaveProperty('hits', 1);
   })
 
+  test('gameboard is updated correctly when a ship is hit', () => {
+    expect(gameBoardOne.gameboard[1][2]).toBe('hit');
+  })
+
+  test('misses also get added to the gameboard', () => {
+    gameBoardOne.receiveAttack([5,6]);
+    expect(gameBoardOne.gameboard[5][6]).toBe('miss');
+    expect(gameBoardOne.shipPositions[5][6]).toBe(null);
+  })
+  
 })
 
